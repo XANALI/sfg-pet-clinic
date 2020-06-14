79a07b9856085c4ae3f16668663747717b2a931e
XANALI <53555116+XANALI@users.noreply.github.com>
2020-06-14 22:30:33 +0600

    Implement Interface for Crud Service.

--- pet-clinic-data/src/main/java/kz/xan/sfgpetclinic/service/PetService.java
+++ pet-clinic-data/src/main/java/kz/xan/sfgpetclinic/service/PetService.java
@@ -2,10 +2,5 @@ package kz.xan.sfgpetclinic.service;
 
 import kz.xan.sfgpetclinic.model.Pet;
 
-import java.util.Set;
-
-public interface PetService {
-    Pet findById(Long id);
-    Pet save(Pet pet);
-    Set<Pet> findAll();
+public interface PetService extends CrudService<Pet, Long> {
 }
