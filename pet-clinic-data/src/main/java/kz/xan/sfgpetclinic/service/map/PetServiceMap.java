0e894d0d9c226dd6940de2bdbe1687e5cea51c09
XANALI <53555116+XANALI@users.noreply.github.com>
2020-06-15 15:11:56 +0600

    Implement Map based Services.

---
+++ pet-clinic-data/src/main/java/kz/xan/sfgpetclinic/service/map/PetServiceMap.java
@@ -0,0 +1,34 @@
+package kz.xan.sfgpetclinic.service.map;
+
+import kz.xan.sfgpetclinic.model.Pet;
+import kz.xan.sfgpetclinic.service.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Pet save(Pet pet) {
+        return super.save(pet.getId(), pet);
+    }
+
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Pet pet) {
+        super.delete(pet);
+    }
+}
---
+++ pet-clinic-data/src/main/java/kz/xan/sfgpetclinic/service/map/VetServiceMap.java
@@ -0,0 +1,34 @@
+package kz.xan.sfgpetclinic.service.map;
+
+import kz.xan.sfgpetclinic.model.Vet;
+import kz.xan.sfgpetclinic.service.CrudService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Vet save(Vet vet) {
+        return super.save(vet.getId(), vet);
+    }
+
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Vet vet) {
+        super.delete(vet);
+    }
+}
