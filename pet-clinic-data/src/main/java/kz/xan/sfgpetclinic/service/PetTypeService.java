dbc62a0225b01021d5f1a8888220baf173670ca0
XANALI <53555116+XANALI@users.noreply.github.com>
2020-06-21 19:23:14 +0600

    Added PetTypeService and Map Impl.

---
+++ pet-clinic-data/src/main/java/kz/xan/sfgpetclinic/service/PetTypeService.java
@@ -0,0 +1,6 @@
+package kz.xan.sfgpetclinic.service;
+
+import kz.xan.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
---
+++ pet-clinic-data/src/main/java/kz/xan/sfgpetclinic/service/map/PetTypeServiceMap.java
@@ -0,0 +1,36 @@
+package kz.xan.sfgpetclinic.service.map;
+
+import kz.xan.sfgpetclinic.model.PetType;
+import kz.xan.sfgpetclinic.service.PetTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class PetTypeServiceMap extends AbstractMapService<PetType, Long> implements PetTypeService {
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+}
