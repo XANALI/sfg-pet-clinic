89d8e0cc3682527ef82684d5f1c1dce5d93beead
XANALI <53555116+XANALI@users.noreply.github.com>
2020-06-21 20:47:24 +0600

    Added check for Pet and PetType in OwnerService, Pet added into bootstrap.

--- pet-clinic-data/src/main/java/kz/xan/sfgpetclinic/service/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/kz/xan/sfgpetclinic/service/map/OwnerServiceMap.java
@@ -1,13 +1,23 @@
 package kz.xan.sfgpetclinic.service.map;
 
 import kz.xan.sfgpetclinic.model.Owner;
+import kz.xan.sfgpetclinic.model.Pet;
 import kz.xan.sfgpetclinic.service.OwnerService;
+import kz.xan.sfgpetclinic.service.PetService;
+import kz.xan.sfgpetclinic.service.PetTypeService;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
 public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService {
+    private final PetTypeService petTypeService;
+    private final PetService petService;
+
+    public OwnerServiceMap(PetTypeService petTypeService, PetService petService) {
+        this.petTypeService = petTypeService;
+        this.petService = petService;
+    }
 
     @Override
     public Owner findById(Long id) {
@@ -16,7 +26,27 @@ public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements 
 
     @Override
     public Owner save(Owner owner) {
-        return super.save(owner);
+        if(owner != null){
+            if(owner.getPets() != null){
+                owner.getPets().forEach(pet -> {
+                    if(pet.getPetType() != null){
+                        if(pet.getPetType().getId() == null){
+                            pet.setPetType(petTypeService.save(pet.getPetType()));
+                        }
+                    }else{
+                        throw new RuntimeException("Pet Type can not be null");
+                    }
+
+                    if(pet.getId() == null){
+                        Pet savedPet = petService.save(pet);
+                        pet.setId(savedPet.getId());
+                    }
+                });
+            }
+            return super.save(owner);
+        }else{
+            return null;
+        }
     }
 
     @Override
--- pet-clinic-web/src/main/java/kz/xan/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/kz/xan/sfgpetclinic/bootstrap/DataLoader.java
@@ -1,25 +1,40 @@
 package kz.xan.sfgpetclinic.bootstrap;
 
 import kz.xan.sfgpetclinic.model.Owner;
+import kz.xan.sfgpetclinic.model.Pet;
+import kz.xan.sfgpetclinic.model.PetType;
 import kz.xan.sfgpetclinic.model.Vet;
 import kz.xan.sfgpetclinic.service.OwnerService;
+import kz.xan.sfgpetclinic.service.PetTypeService;
 import kz.xan.sfgpetclinic.service.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        dog.setName("Cat");
+        PetType savedCatPetType = petTypeService.save(cat);
+
         Owner owner = new Owner();
         owner.setFirstName("Alikhan");
         owner.setLastName("Ryskhan");
@@ -29,6 +44,16 @@ public class DataLoader implements CommandLineRunner {
         Owner owner1 = new Owner();
         owner1.setFirstName("Zhalgas");
         owner1.setLastName("Sarsen");
+        owner1.setAddress("123 Brickerel");
+        owner1.setCity("Miami");
+        owner1.setTelephone("1231231234");
+
+        Pet mikesPet = new Pet();
+        mikesPet.setPetType(savedDogPetType);
+        mikesPet.setOwner(owner1);
+        mikesPet.setBirthDate(LocalDate.now());
+        mikesPet.setName("Rosco");
+        owner1.getPets().add(mikesPet);
 
         ownerService.save(owner1);
 
