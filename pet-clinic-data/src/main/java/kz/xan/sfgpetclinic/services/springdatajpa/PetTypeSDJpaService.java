e69ecd3a13c14478d5eff5d107f35dd1319fa1ee
XANALI <53555116+XANALI@users.noreply.github.com>
2020-06-25 15:15:06 +0600

    Added SD jpa service for Pet Entity.

--- pet-clinic-data/src/main/java/kz/xan/sfgpetclinic/services/springdatajpa/PetTypeSDJpaService.java
+++ pet-clinic-data/src/main/java/kz/xan/sfgpetclinic/services/springdatajpa/PetTypeSDJpaService.java
@@ -3,10 +3,14 @@ package kz.xan.sfgpetclinic.services.springdatajpa;
 import kz.xan.sfgpetclinic.model.PetType;
 import kz.xan.sfgpetclinic.repositories.PetTypeRepository;
 import kz.xan.sfgpetclinic.services.PetTypeService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Service
+@Profile("springdatajpa")
 public class PetTypeSDJpaService implements PetTypeService {
 
     private final PetTypeRepository petTypeRepository;
